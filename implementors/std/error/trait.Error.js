b1d5b878fa34364acd20661a0a26078c75255828
Anthony Nowell <anowell@gmail.com>
2016-07-29 00:57:32 -0600

    update docs

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["wkhtmltopdf"] = [];
+implementors["wkhtmltopdf"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='wkhtmltopdf/enum.Error.html' title='wkhtmltopdf::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
